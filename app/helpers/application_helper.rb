18491b4d4f1a60df10699f2aee807a3463cea667
Giovanni Benussi <giovanni.benussi@usach.cl>
2014-06-12 11:04:05 -0400

    Vista Index y Editar Compuestos funcionando bien, falta agregar seccion para agregar productos basicos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
 	def page_title(page_title)
 		content_for(:page_title) { page_title }
 	end
+	def boton_guardar_cancelar(aceptar_btn, cancelar_url)
+		html  = '<div class="btn-group">'
+		#html += '<button type="submit" class="btn btn-success">Guardar</button>'
+		html += aceptar_btn
+		html += '<a href="' + cancelar_url + '" class="btn btn-danger">Cancelar</a>'
+		html += '</div>'
+		html.html_safe
+	end
 end
