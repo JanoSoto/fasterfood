2853af0f4ea0aaad15bc1509ef2d8111b27b2e17
Giovanni Benussi <giovanni.benussi@usach.cl>
2014-06-12 16:06:26 -0400

    Producto compuesto muestra los basicos. Creada restriccion en modelo compuestos para asegurar que solo existan productos basicos unicos en la relacion con los compuestos

--- app/models/compuesto.rb
+++ app/models/compuesto.rb
@@ -1,5 +1,5 @@
 class Compuesto < ActiveRecord::Base
 	has_many :composicion
-	# distincs es para decir que un compuesto solo puede tener productos basicos unicos
-	has_many :basico, -> { distincs }, :through => :composicion
+	# distinct es para decir que un compuesto solo puede tener productos basicos unicos
+	has_many :basico, -> { distinct }, :through => :composicion
 end
